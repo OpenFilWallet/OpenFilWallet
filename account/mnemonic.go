8ae3e81ca13fa28f9a5946805e1930c390a4e36d
llifezou <llifezou@gmail.com>
2022-11-06 09:49:20 +0800

    add account

---
+++ account/mnemonic.go
@@ -0,0 +1,67 @@
+package account
+
+import (
+	"crypto/subtle"
+	"errors"
+	"github.com/OpenFilWallet/OpenFilWallet/crypto"
+	"github.com/OpenFilWallet/OpenFilWallet/datastore"
+	"github.com/OpenFilWallet/OpenFilWallet/lib/hd"
+)
+
+func GenerateMnemonic(walletDB datastore.WalletDB, mType hd.MnemonicType, passwordKey []byte) error {
+	mnemonic, err := hd.NewMnemonic(mType)
+	if err != nil {
+		return err
+	}
+
+	encryptMnemonic, err := crypto.Encrypt([]byte(mnemonic), passwordKey)
+	if err != nil {
+		return err
+	}
+
+	err = walletDB.SetMnemonic(&datastore.HdWallet{
+		Mnemonic:     encryptMnemonic,
+		MnemonicHash: crypto.Hash256(encryptMnemonic),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ImportMnemonic(walletDB datastore.WalletDB, mnemonic string, passwordKey []byte) error {
+	// check mnemonic
+	good := hd.CheckMnemonic(mnemonic)
+	if !good {
+		return errors.New("invalid mnemonic")
+	}
+
+	encryptedMnemonic, err := crypto.Encrypt([]byte(mnemonic), passwordKey)
+	if err != nil {
+		return err
+	}
+
+	return walletDB.SetMnemonic(&datastore.HdWallet{
+		Mnemonic:     encryptedMnemonic,
+		MnemonicHash: crypto.Hash256(encryptedMnemonic),
+	})
+}
+
+func LoadMnemonic(walletDB datastore.WalletDB, passwordKey []byte) (string, error) {
+	hdWallet, err := walletDB.GetMnemonic()
+	if err != nil {
+		return "", err
+	}
+
+	mnemonic, err := crypto.Decrypt(hdWallet.Mnemonic, passwordKey)
+	if err != nil {
+		return "", err
+	}
+
+	if subtle.ConstantTimeCompare(hdWallet.MnemonicHash, crypto.Hash256(hdWallet.Mnemonic)) != 1 {
+		err = errors.New("warning, abnormal mnemonic check, possible data corruption")
+	}
+
+	return string(mnemonic), err
+}
---
+++ account/privatekey.go
@@ -0,0 +1,181 @@
+package account
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/OpenFilWallet/OpenFilWallet/crypto"
+	"github.com/OpenFilWallet/OpenFilWallet/datastore"
+	"github.com/OpenFilWallet/OpenFilWallet/lib/hd"
+	"github.com/OpenFilWallet/OpenFilWallet/lib/sigs"
+	_ "github.com/OpenFilWallet/OpenFilWallet/lib/sigs/bls"
+	_ "github.com/OpenFilWallet/OpenFilWallet/lib/sigs/secp"
+	filcrypto "github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/wallet/key"
+	"golang.org/x/xerrors"
+	"strings"
+)
+
+func GeneratePrivateKey(walletDB datastore.WalletDB, mnemonic string, sigType filcrypto.SigType, passwordKey []byte) (*key.Key, error) {
+	keyType, err := sigType.Name()
+	if err != nil {
+		return nil, err
+	}
+
+	seed, err := hd.GenerateSeedFromMnemonic(mnemonic, "")
+	if err != nil {
+		return nil, err
+	}
+
+	index, err := walletDB.NextMnemonicIndex()
+	if err != nil {
+		return nil, err
+	}
+
+	path := hd.FILPath(index)
+	extendSeed, err := hd.GetExtendSeedFromPath(path, seed)
+	if err != nil {
+		return nil, err
+	}
+
+	pk, err := sigs.Generate(sigType, extendSeed)
+	if err != nil {
+		return nil, err
+	}
+
+	ki := types.KeyInfo{
+		Type:       types.KeyType(keyType),
+		PrivateKey: pk,
+	}
+
+	privateKey, err := json.Marshal(ki)
+	if err != nil {
+		return nil, err
+	}
+
+	encryptedPrivateKey, err := crypto.Encrypt(privateKey, passwordKey)
+	if err != nil {
+		return nil, err
+	}
+
+	nk, err := key.NewKey(ki)
+	if err != nil {
+		return nil, err
+	}
+
+	err = walletDB.SetPrivate(&datastore.PrivateWallet{
+		PriKey:  encryptedPrivateKey,
+		Address: nk.Address.String(),
+		KeyHash: crypto.Hash256(encryptedPrivateKey),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nk, nil
+}
+
+func ImportPrivateKey(walletDB datastore.WalletDB, priKey, keyFormat string, passwordKey []byte) error {
+	ki, err := GenerateKeyInfoFromPriKey(priKey, keyFormat)
+	if err != nil {
+		return err
+	}
+
+	privateKey, err := json.Marshal(ki)
+	if err != nil {
+		return err
+	}
+
+	encryptedPrivateKey, err := crypto.Encrypt(privateKey, passwordKey)
+	if err != nil {
+		return err
+	}
+
+	nk, err := key.NewKey(*ki)
+	if err != nil {
+		return err
+	}
+
+	return walletDB.SetPrivate(&datastore.PrivateWallet{
+		PriKey:  encryptedPrivateKey,
+		Address: nk.Address.String(),
+		KeyHash: crypto.Hash256(encryptedPrivateKey),
+	})
+}
+
+func LoadPrivateKey(walletDB datastore.WalletDB, passwordKey []byte) ([]key.Key, error) {
+	privateWallets, err := walletDB.WalletList()
+	if err != nil {
+		return nil, err
+	}
+
+	var keys = make([]key.Key, 0)
+	for _, wallet := range privateWallets {
+		var ki types.KeyInfo
+
+		decryptKey, err := crypto.Decrypt(wallet.PriKey, passwordKey)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal(decryptKey, &ki)
+		if err != nil {
+			return nil, err
+		}
+
+		nk, err := key.NewKey(ki)
+		if err != nil {
+			return nil, err
+		}
+
+		keys = append(keys, *nk)
+	}
+
+	return keys, nil
+}
+
+func GenerateKeyInfoFromPriKey(priKey, keyFormat string) (*types.KeyInfo, error) {
+	var ki types.KeyInfo
+	switch keyFormat {
+	case "hex-lotus":
+		data, err := hex.DecodeString(strings.TrimSpace(priKey))
+		if err != nil {
+			return nil, err
+		}
+
+		if err := json.Unmarshal(data, &ki); err != nil {
+			return nil, err
+		}
+	case "json-lotus":
+		if err := json.Unmarshal([]byte(priKey), &ki); err != nil {
+			return nil, err
+		}
+	case "gfc-json":
+		var f struct {
+			KeyInfo []struct {
+				PrivateKey []byte
+				SigType    int
+			}
+		}
+		if err := json.Unmarshal([]byte(priKey), &f); err != nil {
+			return nil, xerrors.Errorf("failed to parse go-filecoin key: %s", err)
+		}
+
+		gk := f.KeyInfo[0]
+		ki.PrivateKey = gk.PrivateKey
+		switch gk.SigType {
+		case 1:
+			ki.Type = types.KTSecp256k1
+		case 2:
+			ki.Type = types.KTBLS
+		default:
+			return nil, fmt.Errorf("unrecognized key type: %d", gk.SigType)
+		}
+	default:
+		return nil, fmt.Errorf("unrecognized format: %s", keyFormat)
+	}
+
+	return &ki, nil
+}
