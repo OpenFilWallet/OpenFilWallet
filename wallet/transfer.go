879197966b369fc326f08ef226cf10de217aedcb
llifezou <llifezou@gmail.com>
2022-12-06 22:34:56 +0800

    add wallet server: transfer

---
+++ wallet/transfer.go
@@ -0,0 +1,33 @@
+package wallet
+
+import (
+	"github.com/OpenFilWallet/OpenFilWallet/chain"
+	"github.com/OpenFilWallet/OpenFilWallet/client"
+	"github.com/OpenFilWallet/OpenFilWallet/modules/buildmessage"
+	"github.com/gin-gonic/gin"
+)
+
+// Transfer Post
+func (w *Wallet) Transfer(c *gin.Context) {
+	param := client.TransferRequest{}
+	err := c.BindJSON(&param)
+	if err != nil {
+		ReturnError(c, ParamErr)
+		return
+	}
+
+	msg, err := buildmessage.NewTransferMessage(w.node.Api, param.BaseParams, param.From, param.To, param.Amount)
+	if err != nil {
+		ReturnError(c, NewError(500, err.Error()))
+		return
+	}
+
+	myMsg, err := chain.EncodeMessage(msg, nil)
+	if err != nil {
+		ReturnError(c, NewError(500, err.Error()))
+		return
+	}
+
+	ReturnOk(c, myMsg)
+	return
+}
