77e57b9ad1b37dcf3cb1a09647d7b7e4dfb43b37
llifezou <llifezou@gmail.com>
2022-12-06 22:36:13 +0800

    add middleware

---
+++ wallet/middleware.go
@@ -0,0 +1,71 @@
+package wallet
+
+import (
+	"fmt"
+	"github.com/OpenFilWallet/OpenFilWallet/modules/app"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Errorf("panic recover err: %v", err)
+
+				ReturnError(c, NewError(500, fmt.Sprintf("panic recover err: %v", err)))
+				c.Abort()
+			}
+		}()
+		c.Next()
+	}
+}
+
+func (w *Wallet) MustUnlock() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if w.lock {
+			ReturnError(c, NewError(500, "wallet is locked"))
+			return
+		}
+
+		// Reset lock Ticker
+		w.unlock()
+
+		c.Next()
+	}
+}
+
+func (w *Wallet) MustHaveNode() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if strings.Contains(c.Request.URL.String(), "wallet") ||
+			strings.Contains(c.Request.URL.String(), "miner") ||
+			strings.Contains(c.Request.URL.String(), "msig") ||
+			strings.Contains(c.Request.URL.String(), "transfer") {
+			if w.node == nil {
+				ReturnError(c, NewError(504, "no node available"))
+				c.Abort()
+				return
+			}
+		}
+
+		c.Next()
+	}
+}
+
+func (w *Wallet) JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if !strings.Contains(c.Request.URL.String(), "login") {
+			token := c.GetHeader("Authorization")
+
+			allow, err := app.AuthVerify(token)
+			if err != nil {
+				ReturnError(c, NewError(505, err.Error()))
+				c.Abort()
+				return
+			}
+			_ = allow
+		}
+
+		c.Next()
+	}
+}
