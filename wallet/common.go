eed44c06953117a30c4bc0daa86f34a89e595c80
llifezou <llifezou@gmail.com>
2023-01-23 11:44:23 +0800

    use client.Response

--- wallet/common.go
+++ wallet/common.go
@@ -1,32 +1,14 @@
 package wallet
 
 import (
-	"fmt"
 	"github.com/OpenFilWallet/OpenFilWallet/client"
 	"github.com/filecoin-project/go-address"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-type Error struct {
-	code int    `json:"code"`
-	msg  string `json:"msg"`
-}
-
-func NewError(code int, msg string) *Error {
-	return &Error{code: code, msg: msg}
-}
-
-func (e *Error) Code() int {
-	return e.code
-}
-
-func (e *Error) Msg() string {
-	return e.msg
-}
-
-func (e *Error) Error() string {
-	return fmt.Sprintf("code：%d, msg:：%s", e.Code(), e.Msg())
+func NewError(code int, msg string) *client.Response {
+	return &client.Response{Code: code, Message: msg}
 }
 
 var (
@@ -45,13 +27,8 @@ func ReturnOk(c *gin.Context, data interface{}) {
 	c.JSON(http.StatusOK, data)
 }
 
-func ReturnError(c *gin.Context, err *Error) {
-	data := client.Response{
-		Code:    err.code,
-		Message: err.msg,
-	}
-
-	c.JSON(http.StatusOK, data)
+func ReturnError(c *gin.Context, res *client.Response) {
+	c.JSON(http.StatusOK, res)
 }
 
 func addr2Str(addrs []address.Address) []string {
